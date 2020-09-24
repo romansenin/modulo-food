1e8b525373a95ed36b487ce4a1110ea15e461b5b
Roman <romansenin15@gmail.com>
2020-09-24 16:51:12 -0700

    added logic to read price from food data

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,18 @@
 const foods = [
-  { name: "Bread", filename: "bread.jpg" },
-  { name: "Eggs", filename: "eggs.png" },
-  { name: "Milk", filename: "milk.png" },
-  { name: "Steak", filename: "steak-pork.png" },
-  { name: "Pizza", filename: "pizza.png" },
+  { name: "Bread", filename: "bread.jpg", price: "10.99" },
+  { name: "Eggs", filename: "eggs.png", price: "11.99" },
+  { name: "Milk", filename: "milk.png", price: "12.99" },
+  { name: "Steak", filename: "steak-pork.png", price: "13.99" },
+  { name: "Pizza", filename: "pizza.png", price: "14.99" }
 ];
 
 document.querySelectorAll(".food-item").forEach((foodItem, index) => {
   const foodImg = foodItem.querySelector(".food-img");
   const foodName = foodItem.querySelector(".food-info .food-name");
-  foodImg.innerHTML = `<img src="./assets/images/${foods[index].filename}" alt="">`;
+  const foodPrice = foodItem.querySelector(".food-price");
+  console.log(foodPrice);
+  foodPrice.querySelector(".price-dollar").textContent = foods[index].price.split(".")[0];
+  foodPrice.querySelector(".price-cent").textContent = foods[index].price.split(".")[1];
+  foodImg.innerHTML = `<img src="./assets/images/${foods[index].filename}" alt="${foods[index].name}">`;
   foodName.textContent = foods[index].name;
 });
