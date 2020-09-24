a8cd22a0ffeca506cdcb2eac42247c2670f5b9af
Roman <romansenin15@gmail.com>
2020-09-24 16:58:58 -0700

    moved food data into separate js file

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,7 @@
-const foods = [
-  { name: "Bread", filename: "bread.jpg", price: "10.99" },
-  { name: "Eggs", filename: "eggs.png", price: "11.99" },
-  { name: "Milk", filename: "milk.png", price: "12.99" },
-  { name: "Steak", filename: "steak-pork.png", price: "13.99" },
-  { name: "Pizza", filename: "pizza.png", price: "14.99" }
-];
-
 document.querySelectorAll(".food-item").forEach((foodItem, index) => {
   const foodImg = foodItem.querySelector(".food-img");
   const foodName = foodItem.querySelector(".food-info .food-name");
   const foodPrice = foodItem.querySelector(".food-price");
-  console.log(foodPrice);
   foodPrice.querySelector(".price-dollar").textContent = foods[index].price.split(".")[0];
   foodPrice.querySelector(".price-cent").textContent = foods[index].price.split(".")[1];
   foodImg.innerHTML = `<img src="./assets/images/${foods[index].filename}" alt="${foods[index].name}">`;
---
+++ assets/js/food.js
@@ -0,0 +1,7 @@
+const foods = [
+  { name: "Bread", filename: "bread.jpg", price: "10.99" },
+  { name: "Eggs", filename: "eggs.png", price: "11.99" },
+  { name: "Milk", filename: "milk.png", price: "12.99" },
+  { name: "Steak", filename: "steak-pork.png", price: "13.99" },
+  { name: "Pizza", filename: "pizza.png", price: "14.99" },
+];
