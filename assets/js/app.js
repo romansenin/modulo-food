52d3d022f785cca97f815d149a6a679b10474711
Roman <romansenin15@gmail.com>
2020-09-30 14:37:29 -0700

    renamed displayFood function, further cleanup

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,23 +2,24 @@ const foodItems = document.querySelector(".food-items");
 let index = 4;
 
 document.querySelectorAll(".food-item").forEach((foodItem, index) => {
-  displayFood(foodItem, index);
+  insertFoodInfo(foodItem, index);
 });
 
-function displayFood(foodItem, index) {
+function insertFoodInfo(foodItem, index) {
   const foodImg = foodItem.querySelector(".food-img");
   const foodName = foodItem.querySelector(".food-info .food-name");
   const foodPrice = foodItem.querySelector(".food-price");
   const price = foods[index].price;
 
   foodPrice.querySelector(".price-dollar").textContent = price.split(".")[0];
-  foodPrice.querySelector(".price-cent").textContent = "." + price.split(".")[1];
+  foodPrice.querySelector(".price-cent").textContent =
+    "." + price.split(".")[1];
   foodImg.innerHTML = `<img src="./assets/images/${foods[index].filename}" alt="${foods[index].name}">`;
   foodName.textContent = foods[index].name;
 }
 
 // fades element from view and removes it from DOM, duration should match CSS transition duration for element's opacity
-async function rollWheel(element, duration, newElement, left) {
+function rollWheel(element, newElement, duration, left) {
   element.style.opacity = "0";
   return new Promise((resolve) => {
     setTimeout(() => {
@@ -35,7 +36,7 @@ document.querySelector(".arrow-left").addEventListener("click", () => {
   const foodBoxes = document.querySelectorAll(".food-box");
   const clone = foodBoxes[0].cloneNode(true);
   index = index < 0 ? foods.length - 1 : index;
-  displayFood(clone, index++ % foods.length); // hence the name of the site
+  insertFoodInfo(clone, index++ % foods.length); // hence the name of the site
   rollWheel(foodBoxes[0], 20, clone, 1);
 
   for (let i = 1; i < foodBoxes.length; i++) {
@@ -49,7 +50,7 @@ document.querySelector(".arrow-right").addEventListener("click", () => {
   const foodBoxes = document.querySelectorAll(".food-box");
   const clone = foodBoxes[0].cloneNode(true);
   index = index < 0 ? foods.length - 1 : index;
-  displayFood(clone, index-- % foods.length);
+  insertFoodInfo(clone, index-- % foods.length);
   rollWheel(foodBoxes[foodBoxes.length - 1], 40, clone, 0);
 
   for (let i = 0; i < foodBoxes.length; i++) {
