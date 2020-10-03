724838fda6913cc8fcc1f704f7f86d6b476d1e3a
Roman <romansenin15@gmail.com>
2020-10-03 15:42:18 -0700

    assigned each food data item an id

--- assets/js/food.js
+++ assets/js/food.js
@@ -1,7 +1,7 @@
 const foods = [
-  { name: "Bread", filename: "bread.jpg", price: "3.00" },
-  { name: "Eggs", filename: "eggs.png", price: "2.50" },
-  { name: "Milk", filename: "milk.png", price: "1.79" },
-  { name: "Steak", filename: "steak-pork.png", price: "10.39" },
-  { name: "Pizza", filename: "pizza.png", price: "14.99" },
+  { id: 1, name: "Bread", filename: "bread.jpg", price: "3.00" },
+  { id: 2, name: "Eggs", filename: "eggs.png", price: "2.50" },
+  { id: 3, name: "Milk", filename: "milk.png", price: "1.79" },
+  { id: 4, name: "Steak", filename: "steak-pork.png", price: "10.39" },
+  { id: 5, name: "Pizza", filename: "pizza.png", price: "14.99" },
 ];
