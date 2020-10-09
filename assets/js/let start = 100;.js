360c1919821b74f809e47004f6f0ae7c41cfbd72
Roman <romansenin15@gmail.com>
2020-10-08 17:39:13 -0700

    removed test js file

--- assets/js/let start = 100;.js
+++
@@ -1,9 +0,0 @@
-let start = 100;
-const result = [];
-for (let i = 1; i <= 5; i++) {
-  start = 100 * (1 / 5);
-  result.push(start);
-  console.log(start);
-}
-
-console.log(result.reduce((a, b) => a + b, 0));
